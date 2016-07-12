2354019339afa78e97ef3586e79664593a077e45
RS Saranyaraj <rs.saranyaraj@gmail.com>
2016-07-12 17:25:04 +0530

    Add 01-Reverse-a-String.js

---
+++ Basic-Algorithm-Scripting/01-Reverse-a-String.js
@@ -0,0 +1,11 @@
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+/*
+// tests
+console.log(reverseString("hello"));
+console.log(reverseString("Howdy"));
+console.log(reverseString("Greatings from Earth"));
+*/
