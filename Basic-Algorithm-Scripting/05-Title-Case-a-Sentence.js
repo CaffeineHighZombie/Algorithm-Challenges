8f94c6a6d6020674308679b575ebc2b41cfb0bf3
RS Saranyaraj <rs.saranyaraj@gmail.com>
2016-07-14 18:36:48 +0530

    Add 05-Title-Case-a-Sentence.js

---
+++ Basic-Algorithm-Scripting/05-Title-Case-a-Sentence.js
@@ -0,0 +1,24 @@
+/* 
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+*/
+
+function titleCase(str) {
+  if (typeof str === 'string') {
+    return str.toLowerCase().split(' ').map(function(element) {
+      elementArr = element.split('');
+      elementArr[0] = elementArr[0].toUpperCase();
+      return elementArr.join('');
+    }).join(' ');
+  } else {
+    return null;
+  }
+}
+
+// tests
+console.log(titleCase("I'm a little tea pot") === "I'm A Little Tea Pot");
+console.log(titleCase("sHoRt AnD sToUt") === "Short And Stout");
+console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") === "Here Is My Handle Here Is My Spout");
+console.log(titleCase(978.65) === null);
+console.log(titleCase(true) === null);
