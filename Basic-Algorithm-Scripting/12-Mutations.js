92048510a7d3a258abf8a464b006212b1b2c0632
RS Saranyaraj <rs.saranyaraj@gmail.com>
2016-07-19 18:48:16 +0530

    Add 12-Mutations.js

---
+++ Basic-Algorithm-Scripting/12-Mutations.js
@@ -0,0 +1,30 @@
+/*
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+*/
+
+function mutation(arr) {
+  if (typeof arr[0] === 'string' && typeof arr[1] === 'string') {
+    return arr[1].toLowerCase().split('').every(function(element) {
+      return arr[0].toLowerCase().indexOf(element) != -1;
+    });
+  } else {
+    return null;
+  }
+}
+
+// tests
+console.log(mutation(["hello", "hey"]) === false);
+console.log(mutation(["hello", "Hello"]) === true);
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) === true);
+console.log(mutation(["Mary", "Army"]) === true);
+console.log(mutation(["Mary", "Aarmy"]) === true);
+console.log(mutation(["Alien", "line"]) === true);
+console.log(mutation(["floor", "for"]) === true);
+console.log(mutation(["hello", "neo"]) === false);
+console.log(mutation(["voodoo", "no"]) === false);
