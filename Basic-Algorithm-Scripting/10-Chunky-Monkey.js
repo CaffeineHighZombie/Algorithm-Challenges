65efa8af739e50033f2a7dc52589912378851556
RS Saranyaraj <rs.saranyaraj@gmail.com>
2016-07-19 13:22:36 +0530

    Correct, working solution to 10-Chunky-Monkey.js

--- Basic-Algorithm-Scripting/10-Chunky-Monkey.js
+++ Basic-Algorithm-Scripting/10-Chunky-Monkey.js
@@ -1,14 +1,20 @@
+/*
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+*/
 
 function chunkArrayInGroups(arr, size) {
-  var finalArr = [];
-  for (var j = 0; j < arr.length / size; j++) {
-    var resultArr = [];
-    for (var i = 0; i < size; i++) {
-      resultArr[i] = arr[j * size + i];
-    }
-    finalArr.push(resultArr);
+  var chunkArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+  	chunkArr.push(arr.slice(i, i+size));
   }
-  return finalArr;
+  return chunkArr;
 }
 
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
+// tests
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));  // [["a", "b"], ["c", "d"]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));  // [[0, 1, 2], [3, 4, 5]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));  // [[0, 1], [2, 3], [4, 5]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));  // [[0, 1, 2, 3], [4, 5]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));  // [[0, 1, 2], [3, 4, 5], [6]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));  // [[0, 1, 2, 3], [4, 5, 6, 7], [8]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));  // [[0, 1], [2, 3], [4, 5], [6, 7], [8]]
